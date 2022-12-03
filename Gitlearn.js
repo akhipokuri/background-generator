1f58aef2a246183a556280e8e2a0115c9dd9ea15
akhipokuri <pokuridev@gmail.com>
2022-12-03 07:33:43 -0700

    updating the file with basic commands

---
+++ Gitlearn.js
@@ -0,0 +1,8 @@
+// Clone : git clone https://github.com/akhipokuri/background-generator.git
+      // : .git folder will be created after cloning which will allow user to communicate with github.
+// Status : git status - to check the status of the files in the project folder created in the local.
+     // : User will see untracked files in the project local folder.
+// git add : git add files.js - we use this to include the untracked files to include in the commit.
+// git commit : git commit -m "commit comments" - this one is used to commit the code with comments.
+        // : We use git commit to commit meaning to lock the changes.
+// git push : git push - finally we use it to let the github.com about the changes.    
