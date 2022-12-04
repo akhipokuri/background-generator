1c38ec11d7cb864d43c49ba574eaebe12ac7569c
akhipokuri <pokuridev@gmail.com>
2022-12-04 07:06:54 -0700

    reverting back to orginal title

--- Gitlearn.js
+++ Gitlearn.js
@@ -1,8 +1,16 @@
-// Clone : git clone https://github.com/akhipokuri/background-generator.git
-      // : .git folder will be created after cloning which will allow user to communicate with github.
-// Status : git status - to check the status of the files in the project folder created in the local.
-     // : User will see untracked files in the project local folder.
-// git add : git add files.js - we use this to include the untracked files to include in the commit.
-// git commit : git commit -m "commit comments" - this one is used to commit the code with comments.
-        // : We use git commit to commit meaning to lock the changes.
-// git push : git push - finally we use it to let the github.com about the changes.    
+           // Clone : git clone https://github.com/akhipokuri/background-generator.git
+                 // : .git folder will be created after cloning which will allow user to communicate with github.
+          // Status : git status - to check the status of the files in the project folder created in the local.
+                 // : User will see untracked files in the project local folder.
+         // git add : git add files.js - we use this to include the untracked files to include in the commit.
+      // git commit : git commit -m "commit comments" - this one is used to commit the code with comments.
+                 // : We use git commit to commit meaning to lock the changes.
+        // git push : git push - finally we use it to let the github.com about the changes.    
+        // git pull : git pull - to pull the code from github.
+      // git branch : git branch - it will show the available branches.
+      // git branch : git branch little feature - it will create a new branch called little feature.
+    // git checkout : git checkout little feature - It will help to switch over little feature branch. 
+// up stream branch : git push -u origin littlefeature - we are creating littile/origin remote branch for little feature.
+console.log("print this");
+// ovhfoivfdoihoihndfoijn
+
